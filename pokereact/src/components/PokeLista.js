45f39fef27d4208c14a4010a5bd796c3158770c9
SKMonarch <christiansanlu@gmail.com>
2025-01-09 07:12:09 +0100

    Redireccion Navigate

--- pokereact/src/components/PokeLista.js
+++ pokereact/src/components/PokeLista.js
@@ -1,7 +1,10 @@
 import { useEffect,useState } from "react";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
 const PokeLista = (props) => {
     const [pokemons, setPokemons] = useState([]);
+    const navigate= useNavigate();
 
     useEffect(() => {
         axios.get("https://pokeapi.co/api/v2/pokemon/").then(response =>{
@@ -9,11 +12,21 @@ const PokeLista = (props) => {
         })
     }, [])    
     
+    const funcionNavegarASnivy = () =>{
+        navigate("/pokemon/495");
+    }
     return <div>
+        <button onClick={funcionNavegarASnivy}>Navega a Snivy</button>
+        <Link  to="/pokemon/25"> Ir a PIKACHU</Link>
         <h1>Lista</h1>
-        {pokemons.map(p => <p>Bo Nadal</p>)}
+        {pokemons.map(p => {
+           return <>
+           <p>Este pokemon es {p.name}</p>
+           <div onClick={ () => {navigate("/pokemon/"+p.name)}}>Navegar</div>
+           <Link to={"pokemon/"+p.name}>Navegar</Link>
+           </>
+        })}
     </div>
 }
-
 export default PokeLista;
 
